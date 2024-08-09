8c0650bf484ec147cefb9bc2897d668c42440b37
veniceos <vinicius.canevari1@gmail.br>
2024-08-09 08:51:42 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagína inicial - Gym</title>
+</head>
+<body>
+    <a href="login.php">Entre</a> / <a href="cadastro.php">Cadastre-se</a>
+</body>
+</html>
